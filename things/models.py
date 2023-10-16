d00219fd6be71b5b621b1805dfc154e7b9eaea15
Fardeen Idrus <fardeenidrus123@gmail.com>
2023-10-16 12:05:41 +0100

    configurations for admin interface

--- things/models.py
+++ things/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    name = models.TextField(default = '')
+    description = models.TextField(default = '')
+    quantity = models.PositiveIntegerField(default = 0, validators = [MinValueValidator(0),MaxValueValidator(100)])
+ 
+ 
